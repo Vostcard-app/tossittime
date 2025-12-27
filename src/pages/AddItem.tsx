025aa12eae819b7895d488232812436b5a65e365
jaynbond <info@vostcard.com>
2025-12-27 17:25:30 +0000

    Fix TypeScript error: remove unused useEffect import

--- src/pages/AddItem.tsx
+++ src/pages/AddItem.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useMemo, useEffect } from 'react';
+import React, { useState, useMemo } from 'react';
 import { useNavigate, useSearchParams, useLocation } from 'react-router-dom';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { auth } from '../firebase/firebaseConfig';
